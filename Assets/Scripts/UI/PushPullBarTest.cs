15ae142a188eb0cdba3778747cedd4b70b896e77
MarkNuuut <73965163+MarkNuuut@users.noreply.github.com>
2021-01-16 17:10:37 -0500

    PushPullBar

---
+++ Assets/Scripts/UI/PushPullBarTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PushPullBarTest : MonoBehaviour
+{
+    
+    public void GoodScore()
+    {
+        Events.Instance.InvokeEv("ScoreChange", -.1);
+    }
+
+    public void BadScore()
+    {
+        Events.Instance.InvokeEv("ScoreChange", .1);
+    }
+}
