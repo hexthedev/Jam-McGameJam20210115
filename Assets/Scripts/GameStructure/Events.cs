2355ae085dd60d76b40d206c42b722228416c312
james <jamesmccafferty@live.ca>
2021-01-15 21:02:09 -0500

    Started some really simple structure stuff

---
+++ Assets/Scripts/GameStructure/Events.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+
+public class Events : MonoBehaviour
+{
+    public static Events Instance = null;
+
+    private List<Action<string, object>> Subs = new List<Action<string, object>>();
+
+    public void OnAwake()
+    {
+        Instance = this;
+    }
+
+    public void Subscribe(Action<string, object> callback)
+    {
+        Subs.Add(callback);
+    }
+
+    public void Invoke(string name, object arg)
+    {
+        foreach(Action<string, object> call in Subs)
+        {
+            call(name, arg);
+        }
+    }
+
+}
---
+++ Assets/Scripts/GameStructure/Game.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public static Game Instance = null;
+
+    /// <summary>
+    /// -100 for happy and +100 for angry population
+    /// </summary>
+    public float score;
+
+    public float time;
+
+
+    public void Awake()
+    {
+        Instance = this;
+    }
+
+
+
+    public void Update()
+    {
+        time = +Time.deltaTime;
+    }
+
+
+
+
+}
