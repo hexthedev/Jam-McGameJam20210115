9e95967dd03752f5b1cfafb566d96394ece75523
james <jamesmccafferty@live.ca>
2021-01-17 11:11:43 -0500

    Started the animation controller

---
+++ Assets/AnimationDataFeed.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationDataFeed : MonoBehaviour
+{
+    public Animator anim;
+    public Rigidbody2D rb;
+
+    public void Update()
+    {
+        anim.SetFloat("XVelocity", Mathf.Abs(rb.velocity.x));
+        anim.SetFloat("YVelocity",rb.velocity.y);
+
+    }
+
+}
---
+++ Assets/ScaleFliper.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ScaleFliper : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xVel = rb.velocity.x;
+
+        if(xVel < -0.01f)
+        {
+            transform.localScale = new Vector3(-0.35f, 0.35f, 0.35f);
+        } 
+        else if(xVel > 0.01f)
+        {
+            transform.localScale = new Vector3(0.35f, 0.35f, 0.35f);
+        }
+    }
+}
